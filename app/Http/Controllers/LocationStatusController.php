6f9c6ead7bce7e3d74d616fedb9e428241f34d13
yelmefto <yelmefto@cisco.com>
2021-10-15 16:57:20 +0100

    add location tables

---
+++ app/Http/Controllers/LocationStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LocationStatus;
+use Illuminate\Http\Request;
+
+class LocationStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\LocationStatus  $locationStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LocationStatus $locationStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\LocationStatus  $locationStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LocationStatus $locationStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\LocationStatus  $locationStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LocationStatus $locationStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\LocationStatus  $locationStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LocationStatus $locationStatus)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_10_15_154102_create_location_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('location_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('produit_id');
+            $table->integer('location_id');
+            $table->integer('montant')->default(0);
+            $table->integer('qte')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('location_details');
+    }
+}
